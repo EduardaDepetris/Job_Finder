138789ed7d81083fe602242e8a83ce01bec4af70
EduardaDepetris <dudinhadepetris04@gmail.com>
2025-08-25 11:30:10 -0300

    Feat: configura servidor Express, Handlebars e rota inicial de jobs

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const app = express();
-const db = require('./DB/connection');
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
+const job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended:false}));
+
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //dbConnection
 db
@@ -21,5 +38,38 @@ db
 
 //routes
 app.get('/', (req, res) => {
-    res.send("Está funcionando o site certinho?");
+
+    let search = req.query.job;
+    let query  = '%'+search+'%';
+
+    if(!search){
+
+        job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+
+            res.render('index', {
+                jobs, search
+            });
+        })
+        .catch(err => console.log(err));
+
+    } else {
+        job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {
+
+            res.render('index', {
+                jobs
+            });
+        });
+    }
+    
 });
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
